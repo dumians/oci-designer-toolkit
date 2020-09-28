bd1a1e0c111235dd3ff38f377c8b8c1aa3dc95d4
jdumitru <jdumitru@JDUMITRU-CH.ch.oracle.com>
2020-09-28 06:55:08 +0200

    prototype siebel reference & drawio

---
+++ visualiser/generators/okitDrawioGenerator.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2020, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+"""Provide Module Description
+"""
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+__author__ = ["Johannes Dumitru (Oracle Cloud Architecture & Strategy)"]
+__version__ = "1.0.0"
+__module__ = "ociDrawioGenerator"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+import os
+
+from common.okitLogging import getLogger
+from generators.okitGenerator import OCIGenerator
